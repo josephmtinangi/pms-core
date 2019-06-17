ec3b19b49b0e1cb1988418cda87c8c52ae28a412
Joseph Mtinangi <josephmtinangi@gmail.com>
2019-06-17 15:12:06 +0300

    Setup jwt-auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use JWTAuth;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+	public function register(Request $request)
+	{
+		$this->validate($request, [
+			'email' => 'required|unique:users'
+		]);
+
+	    $user = new User;
+	    $user->email = $request->email;
+	    $user->name = $request->name;
+	    $user->password = bcrypt($request->password);
+	    $user->save();
+	    
+	    return response([
+	        'status' => 'success',
+	        'data' => $user
+	    ], 200);
+	}
+
+	public function login(Request $request)
+	{
+	    $credentials = $request->only('email', 'password');
+	    if ( ! $token = JWTAuth::attempt($credentials)) {
+	            return response([
+	                'status' => 'error',
+	                'error' => 'invalid.credentials',
+	                'message' => 'Invalid Credentials.'
+	            ], 400);
+	    }
+	    $user = User::whereEmail($request->email)->first();
+	    $user->login_count = $user->login_count + 1;
+	    $user->save();
+	    
+	    return response([
+	            	'token' => $token,
+	            	'user' => $user,
+		        ]);
+	}	
+
+	public function user(Request $request)
+	{
+	    $user = User::find(Auth::user()->id);
+	    return response([
+	            'status' => 'success',
+	            'data' => $user
+	        	]);
+	}
+
+	public function refresh()
+	{
+	    return response([
+	            'status' => 'success'
+	        	]);
+	}	
+
+	public function logout()
+	{
+	    JWTAuth::invalidate();
+	    return response([
+	            'status' => 'success',
+	            'message' => 'Logged out Successfully.'
+	        ], 200);
+	}
+
+}
